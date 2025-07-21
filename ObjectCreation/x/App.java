037e626c11edca572a9730ef8e0630748ec1a1c8
david9112 <114740347+david9112@users.noreply.github.com>
2025-07-21 13:54:03 +0530

    Add files via upload

---
+++ ObjectCreation/x/App.java
@@ -0,0 +1,15 @@
+package org.example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  // creation of container as well as the object in container
+        Alien a= (Alien)context.getBean("alien1");
+        a.code();
+
+    }
+}
