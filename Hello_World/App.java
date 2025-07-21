f4d23ab3bf9f4d3cf010706f3fea0ef5b83c039f
david9112 <114740347+david9112@users.noreply.github.com>
2025-07-21 13:46:19 +0530

    Update App.java

--- Hello_World/App.java
+++ Hello_World/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");    // creation of container 
         System.out.println( "Hello World!" ); 
         Alien a= (Alien)context.getBean("alien");
         a.code();
