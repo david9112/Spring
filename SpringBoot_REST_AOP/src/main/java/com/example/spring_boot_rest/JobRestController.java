2a322387eec1f5ed660f96a7d928e36e9a142c5f
david9112 <114740347+david9112@users.noreply.github.com>
2025-07-30 17:57:25 +0530

    Add files via upload

---
+++ SpringBoot_REST_AOP/src/main/java/com/example/spring_boot_rest/JobRestController.java
@@ -0,0 +1,58 @@
+package com.example.spring_boot_rest;
+
+import com.example.spring_boot_rest.model.JobPost;
+import com.example.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+//    @ResponseBody            // sends the data
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable int postId){
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")             // the jobPost url in the annotation are different because it has different mapping methods
+    public void addJob(@RequestBody JobPost jobPost){       // the request obj is json the requestbody converts it
+            service.addJob(jobPost);
+    }
+
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost){
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable int  postId){
+        service.deleteJob(postId);
+        return "Deleted";
+    }
+
+    @GetMapping("load")
+    public String loadData(){
+        service.load();
+
+        return "Success";
+    }
+
+    @GetMapping("jobPosts/keyword/{keyword}")
+    public List<JobPost> searchByKeyword(@PathVariable("keyword") String keyword){
+        return service.search(keyword);
+
+    }
+}
