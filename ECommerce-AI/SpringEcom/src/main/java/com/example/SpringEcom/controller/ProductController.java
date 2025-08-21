1bf3e233b89fb953d1e01d00a152cb0bf5dbf51b
david9112 <114740347+david9112@users.noreply.github.com>
2025-08-21 23:18:46 +0530

    Add files via upload

---
+++ ECommerce-AI/SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -0,0 +1,113 @@
+package com.example.SpringEcom.controller;
+
+import com.example.SpringEcom.model.Product;
+import com.example.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product!=null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addOrUpdateProduct(product,imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        }
+        catch (IOException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CREATED);
+        }
+    }
+
+    @GetMapping("product/{productId}/image")
+    public Object getImageByProductId(@PathVariable int productId){
+
+        Product product1= productService.getProductById(productId);
+
+        if (product1 == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(product1.getImageData(),HttpStatus.OK);
+    }
+
+    @PutMapping("product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id,@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product updatedProduct= null;
+        try
+        {
+            updatedProduct = productService.addOrUpdateProduct(product,imageFile);
+            return new ResponseEntity<>("updated!",HttpStatus.OK);
+        }
+        catch (IOException e)
+        {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        if(productService.getProductById(id) != null)
+        {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("deleted!",HttpStatus.OK);
+        }
+        return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @GetMapping("products/search-results")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("Searching: "+keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping("/product/generate-description")
+     public ResponseEntity<String> generateDescription(@RequestParam String name, @RequestParam String category){
+
+        try{
+            String aiDescription = productService.generateDescription(name,category);
+            return new ResponseEntity<>(aiDescription, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+     }
+
+    @PostMapping("/product/generate-image")
+    public ResponseEntity<?> generateImage(@RequestParam String name, @RequestParam String category,@RequestParam String description){
+
+        try{
+            byte[] aiImage = productService.generateImage(name,category,description);
+            return new ResponseEntity<>(aiImage, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
